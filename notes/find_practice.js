d084d98a39ee241200cfea2ea9cfa75a0a71fe67
Dwaylan <Dwaylan.Applewhite@gmail.com>
2020-08-20 19:05:01 -0500

    added packages

---
+++ notes/find_practice.js
@@ -0,0 +1,12 @@
+// Find returns the value of the first element in the array that meets the critera
+// as opposed to filter which return an array of all elements that satisfy critera
+
+let word = ['broccolli',]
+let letter = word.split('')
+
+let positionFirstVowel = 
+
+
+
+console.log(word)
+console.log(letter)
